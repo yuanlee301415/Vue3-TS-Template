ddce5cf37dc702b7faa62a4a52ab656ce28d9c3b
yuanlee <yuanlee@163.com>
2023-04-22 16:50:24 +0800

    chore: 运行 `format`

--- src/router/types.ts
+++ src/router/types.ts
@@ -1,32 +1,32 @@
-import type { RouteMeta, RouteRecordRaw } from "vue-router";
-import type { DefineComponent } from "vue";
+import type { RouteMeta, RouteRecordRaw } from 'vue-router'
+import type { DefineComponent } from 'vue'
 
 export type Component<T = any> =
   | DefineComponent
-  | (() => Promise<typeof import("*.vue")>)
-  | (() => Promise<T>);
+  | (() => Promise<typeof import('*.vue')>)
+  | (() => Promise<T>)
 
 type _RouteMeta = Partial<{
-  title: string;
-  hiddenMenu: boolean;
-  hiddenChildrenInMenu: boolean;
-}>;
+  title: string
+  hiddenMenu: boolean
+  hiddenChildrenInMenu: boolean
+}>
 
-export type AppRouteMeta = RouteMeta & _RouteMeta;
+export type AppRouteMeta = RouteMeta & _RouteMeta
 
 // @ts-ignore
-export interface AppRouteRecordRaw extends Omit<RouteRecordRaw, "meta"> {
-  name: string;
-  meta: AppRouteMeta;
-  component?: Component | string;
-  components?: Component;
-  children?: AppRouteRecordRaw[];
-  props?: Recordable;
-  fullPath?: string;
+export interface AppRouteRecordRaw extends Omit<RouteRecordRaw, 'meta'> {
+  name: string
+  meta: AppRouteMeta
+  component?: Component | string
+  components?: Component
+  children?: AppRouteRecordRaw[]
+  props?: Recordable
+  fullPath?: string
 }
 
-export type AppRouteModule = AppRouteRecordRaw;
+export type AppRouteModule = AppRouteRecordRaw
 
-declare module "vue-router" {
+declare module 'vue-router' {
   interface RouteMeta extends _RouteMeta {}
 }
