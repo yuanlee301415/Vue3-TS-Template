fe0bcb55034e494632b117cdf7252b9c6b6a8c9a
yuanlee <yuanlee@163.com>
2023-05-14 19:41:51 +0800

    update: 整体布局

--- src/layout/Content/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <RouterView v-slot="{ Component, route }">
-    <transition name="fade-slide" mode="out-in">
-      <div class="container">
-        <component :is="Component" :key="route.path" />
-      </div>
-    </transition>
-  </RouterView>
-</template>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,39 +1,27 @@
 <template>
   <section class="layout">
-    <LayoutSideBar class="sideBar" />
+    <LayoutHeader />
 
     <main>
-      <LayoutHeader />
-      <LayoutContent />
+      <LayoutSideBar />
+
+      <div class="container">
+        <LayoutBreadcrumbs />
+
+        <RouterView v-slot="{ Component, route }">
+          <transition name="fade-slide" mode="out-in">
+            <div class="content">
+              <component :is="Component" :key="route.path" />
+            </div>
+          </transition>
+        </RouterView>
+      </div>
     </main>
   </section>
 </template>
 
 <script lang="ts" setup>
-import LayoutSideBar from './SideBar/index.vue'
 import LayoutHeader from './Header/index.vue'
-import LayoutContent from './Content/index.vue'
+import LayoutSideBar from './SideBar/index.vue'
+import LayoutBreadcrumbs from './Breadcrumbs/index.vue'
 </script>
-
-<style scoped lang="less">
-@sideWidth: 210px;
-
-.layout {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 100%;
-  .sideBar {
-    position: fixed;
-    width: @sideWidth;
-    height: 100vh;
-    background-color: #001529;
-  }
-  main {
-    margin-left: @sideWidth;
-    .container {
-      padding: 10px;
-    }
-  }
-}
-</style>
